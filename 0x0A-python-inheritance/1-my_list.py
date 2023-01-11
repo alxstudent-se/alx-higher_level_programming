d7396ad29b1ea631eee88e36c7e0a6e04ea88889
alxstudent-se <110991136+alxstudent-se@users.noreply.github.com>
2023-01-11 16:54:51 +0000

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/1-my_list.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-""" A class MyList that inherits from list """
-
-class MyList(list):
-    def print_sorted(self):
-        """ prints a sorted list """
-        printed(self.sort())
